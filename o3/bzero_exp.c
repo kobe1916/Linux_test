35d9e8c7a92dee1b473228ff554011a7a162e806
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2020-03-11 19:38:06 +0800

    Create bzero_exp.c

---
+++ o3/bzero_exp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+	char *p2 = (char*)malloc(4*sizeof(char));
+	memset(p2,'\0',4);
+	printf("memset:memory is :%s\n",memset(p2,'a',3));
+	bzero(p2+2，2);
+	printf("bzero:memory is :%s\n",p2);
+	return 0;
+}
